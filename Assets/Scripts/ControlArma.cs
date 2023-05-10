0c821164b9a910882a28ba54780788f196a6f635
MaineEG <mychemical977@gmail.com>
2023-05-10 09:37:29 +0600

    Tarea disparo
    Hacer Disparo

---
+++ Assets/Scripts/ControlArma.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlArma : MonoBehaviour
+{
+    [SerializeField] private Arma arma;
+
+    public void AlDisparar(InputAction.CallbackContext value)
+    {
+        arma.ProcesarEntrada(value.action.triggered);
+    }
+}
+
+
+
